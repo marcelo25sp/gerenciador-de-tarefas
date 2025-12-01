c106868c160347153dc73c074a523b3116b72f56
Marcelo Nascimento <marcelo.ngomes@hotmail.com>
2025-12-01 17:36:43 -0300

    Implementa os atributos da classe Tarefas

--- src/entities/Tarefas.java
+++ src/entities/Tarefas.java
@@ -1,5 +1,17 @@
 package entities;
 
+import java.time.LocalDate;
+
+import entities.enums.StatusTarefa;
+
 public class Tarefas {
+	
+	private Integer id;
+	private String nome;
+	private String descricao;
+	private StatusTarefa status;
+	private LocalDate dataCriacao;
+	private LocalDate dataLimite;
+	
 
 }
