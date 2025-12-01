d0ef2ec9c2f295ec6d8fe79fc9796d0166eb5b90
Marcelo Nascimento <marcelo.ngomes@hotmail.com>
2025-12-01 17:54:25 -0300

    Implementa lista interna e métodos iniciais(ainda vazios) na classe GerenciadorTarefas

--- src/entities/GerenciadorTarefas.java
+++ src/entities/GerenciadorTarefas.java
@@ -1,5 +1,28 @@
 package entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import entities.enums.StatusTarefa;
+
 public class GerenciadorTarefas {
+
+	private List<Tarefas> tarefas = new ArrayList<>();
+
+	public void adicionarTarefa(Tarefas tarefa) {
+
+	}
+
+	public void removerTarefa(Tarefas tarefa) {
+
+	}
+
+	public void listarTarefas() {
+
+	}
+
+	public void listarPorStatus(StatusTarefa status) {
+
+	}
 
 }
