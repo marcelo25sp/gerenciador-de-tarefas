b03d6f54d3e45a6860e3e2118e3700e4747bba49
Marcelo Nascimento <marcelo.ngomes@hotmail.com>
2025-12-01 20:15:41 -0300

    Cria uma instancia da classe GerenciadorTarefas na classe Main

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,8 +1,20 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.GerenciadorTarefas;
+
 public class Main {
 
 	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		GerenciadorTarefas gerenciador = new GerenciadorTarefas();
+		
+		sc.close();
 
 	}
 
