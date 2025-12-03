ffc1e42143f608603d732126971111563a8c4313
Marcelo Nascimento <marcelo.ngomes@hotmail.com>
2025-12-03 20:25:21 -0300

    Refatora e corrige o método listarTarefas na classe GerenciadorTarefas

--- src/entities/GerenciadorTarefas.java
+++ src/entities/GerenciadorTarefas.java
@@ -23,13 +23,13 @@ public class GerenciadorTarefas {
 		}
 		return false;
 	}
-	
+
 	public int getTotalTarefas() {
 		return tarefas.size();
 	}
 
 	public void listarTarefas() {
-		if (tarefas.isEmpty()) { 
+		if (tarefas.isEmpty()) {
 			System.out.println("Nenhuma tarefa cadastrada!");
 		} else {
 			System.out.println("===Todas as tarefas cadastradas===");
@@ -42,19 +42,20 @@ public class GerenciadorTarefas {
 
 	public void listarPorStatus(StatusTarefa status) {
 		boolean encontrado = false;
-		
+
 		System.out.println("===Tarefas com status: " + status + " ===");
 		System.out.println("=========================================");
-		
+
 		for (Tarefas x : tarefas) {
 			if (x.getStatus() == status) {
 				System.out.println(x);
 				encontrado = true;
 			}
-			
-			if(!encontrado) {
-				System.out.println("Nenhuma tarefa encontrada com o status mencionado!");
-			}
+
+		}
+
+		if (!encontrado) {
+			System.out.println("Nenhuma tarefa encontrada com o status mencionado!");
 		}
 
 	}
